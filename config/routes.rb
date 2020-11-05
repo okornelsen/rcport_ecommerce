16b805e1439d8511d0c575e66fa11eeb44caca16
Owen <okornelsen@academic.rrc.ca>
2020-11-05 11:29:51 -0600

    Addition of home page and modified url routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :home, only: %i[index]
+  resources :products, only: %i[index show]
+
+  root to: "home#index"
 end
