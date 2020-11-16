585665c41c6a3f4c6211522b9898537416d37e69
Owen <okornelsen@academic.rrc.ca>
2020-11-15 22:22:39 -0600

    Create pricetype data, add relationships and modify views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,9 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.order("name ASC")
+    @products = Product.includes(:pricetype).order("name ASC")
   end
 
   def show
-    @product = Product.find(params[:id])
+    @product = Product.includes(:pricetype).find(params[:id])
   end
 end
--- app/models/pricetype.rb
+++ app/models/pricetype.rb
@@ -1,3 +1,5 @@
 class Pricetype < ApplicationRecord
   has_many :products
+
+  validates :name, presence: true
 end
