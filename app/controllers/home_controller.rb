f8dddb07fa2c8d8aa1f5aa604c46bbaad82d9c15
Owen <okornelsen@academic.rrc.ca>
2020-11-17 12:07:47 -0600

    Populated Home page views, tweaked other views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @products_on_sale = Product.includes(:pricetype).where("pricetypes.name LIKE 'On Sale'").references(:pricetypes).limit(4)
+    @products_new = Product.includes(:pricetype).where("pricetypes.name LIKE 'New'").references(:pricetypes).order(:created_at).limit(4)
   end
 end
