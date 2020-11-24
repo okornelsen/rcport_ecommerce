3d8b780c5e7f37fd8d78b74aa83b54d42423b5e9
Owen <okornelsen@academic.rrc.ca>
2020-11-24 15:18:24 -0600

    Updated category views to accept proper images Moved New Product Method Check to the Application Controller Initializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,20 +20,6 @@ class ApplicationController < ActionController::Base
     @categories_dropdown = Category.order(:name)
   end
 
-  def update_new_products
-    @new_products = Product.includes(:pricetype).where("pricetypes.name LIKE 'New'").references(:pricetypes)
-    if !@new_products.nil?
-      @base_product_id = Pricetype.where("name LIKE 'Base'").first.id
-      @today = Time.zone.today
-      @new_products.each do |product|
-        @not_new_date = product.created_at + (86_400 * 3)
-        next unless @not_new_date < @today
-        product.pricetype_id = @base_product_id
-        product.save
-      end
-    end
-  end
-
   protected
 
   def configure_permitted_parameters
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,18 @@
 #     https: false
 #   )
 # end
+
+def update_new_products
+  @new_products = Product.includes(:pricetype).where("pricetypes.name LIKE 'New'").references(:pricetypes)
+  unless @new_products.nil?
+    @base_product_id = Pricetype.where("name LIKE 'Base'").first.id
+    @today = Time.zone.today
+    @new_products.each do |product|
+      @not_new_date = product.created_at + (86_400 * 3)
+      next unless @not_new_date < @today
+
+      product.pricetype_id = @base_product_id
+      product.save
+    end
+  end
+end
