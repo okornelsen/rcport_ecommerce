ec4e4b916896357b04c1310b1dd2f0a12fcad87d
Owen <okornelsen@academic.rrc.ca>
2020-11-25 15:45:25 -0600

    Fix ActiveAdmin issue with Devise Login Update province abbreviations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,21 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  attr_writer :login
+
+  def login
+    @login || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(email) = :value", { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
